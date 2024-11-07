290e674312915c68861b2cfb68382a03ed583d6f
unknown <mohanad.13.93@gmail.com>
2024-11-07 11:27:56 +0100

    add comments on rock paper scissors.js

--- assets/js/rock-paper-scissor.js
+++ assets/js/rock-paper-scissor.js
@@ -1,14 +1,27 @@
 let initTypes=['rock','paper','scissor'];
+
+
 let types=document.querySelectorAll('img');
 for(let type of types)
     type.addEventListener('click',function(e){
+        // get type name from the clicked image (rock, paper or scissor)
         let typeName= type.getAttribute('data-type');
+
+        // get a random choice from the array (rock, paper or scissor)
         let randomType=initTypes[Math.floor(Math.random()*3)];
+
+        // the place where the played images are inserted
         let rockPaperScissorArea=document.getElementById("rock-paper-scissor-game");
+
+        // create the first image which is player from the user
         let firstImage=document.createElement('img');
+
+        // create the second image which is a random choice form the computer
         let img1=document.createElement('img');
+        
         switch(typeName)
             {
+                // set all attributes for image that we created before for the user choice 
                 case 'paper':
                 firstImage.setAttribute("src",'assets/images/Paper.png');
                 firstImage.setAttribute("alt",'Paper'); 
@@ -27,6 +40,7 @@ for(let type of types)
             }
             switch(randomType)
             {
+                // set all attributes for image that we created before for the random choice from the computer 
                 case 'paper':
                     img1.setAttribute("src",'assets/images/Paper.png');
                     img1.setAttribute("alt",'Paper');
@@ -47,11 +61,16 @@ for(let type of types)
             }
 
 
+        // initial the previous play
         rockPaperScissorArea.innerHTML='';
+
+        // append our two created images
         rockPaperScissorArea.appendChild(firstImage);
         rockPaperScissorArea.appendChild(img1);
 
 
+        // compare the choised type from the user with the random type from the computer 
+        // and either increase the wins numbers or increase the losts numbers
         if( randomType=='rock' &&typeName=='paper')
         {
             let wins=parseInt(document.getElementById('won').textContent);
